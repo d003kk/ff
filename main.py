a07d9c37fd82333b7072a3cac1b0e13e850a7d29
Ben Sims <benjamin.james.sims@gmail.com>
2021-01-28 21:04:48 +0000

    Added player potions

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import play
 
 myplayer = play.player()
 myplayer.gen_stat()
+
+potions=["Stamina", "Skill", "Strength"]
+print("Choose one adventurer potion")
+for i, f in enumerate(potions, start=1):
+    option = "For {0} press {1}".format(f, i)
+    print(option)
+myplayer.potion = potions[int(input("Enter (1-3)"))]
 myplayer.print_player()
+
+
